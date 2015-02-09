aec57eef8e3cbe59a3013871bf41587dcd6049a1
Joe Watkins <joe@emergeinteractive.com>
2015-02-09 11:42:45 -0800

    Add grunt-svgstore to the project as dev dependency

--- gruntfile.js
+++ gruntfile.js
@@ -11,12 +11,32 @@ module.exports = function(grunt) {
           dest: "fallbacks/"
         }]
       }
+    },
+
+    svgstore: {
+      options: {
+        prefix : 'icon-', // This will prefix each ID
+        svg: {
+          viewBox : '0 0 50 50',
+          style: "display: none;"
+        },
+        cleanup: true,
+        formatting : {
+          indent_size : 2
+        }
+      },
+      default : {
+        files: {
+          'svgdefs.svg': ['svg/*.svg'],
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks("grunt-grunticon");
+  grunt.loadNpmTasks("grunt-svgstore");
 
-  grunt.registerTask("default", ["grunticon"]);
+  grunt.registerTask("default", ["svgstore","grunticon"]);
 
 };
